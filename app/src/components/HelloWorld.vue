98c03997e27484fdd0dbd0f210d5e55f376abb9a
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-02-03 17:02:57 +0000

    最初のコミット

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 defineProps<{
-  msg: string
-}>()
+  msg: string;
+}>();
 </script>
 
 <template>
@@ -11,6 +11,7 @@ defineProps<{
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      What's next?
     </h3>
   </div>
 </template>
