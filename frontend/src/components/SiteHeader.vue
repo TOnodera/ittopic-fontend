44207b5f62b10e267a12461c3249be79e0bd148a
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-02-08 22:00:00 +0000

    ヘッダー修正

--- frontend/src/components/SiteHeader.vue
+++ frontend/src/components/SiteHeader.vue
@@ -7,36 +7,49 @@ defineProps<{
 <template>
   <header>
     <!-- sp -->
-    <div class="columns is-mobile is-hidden-desktop pl-5 pt-1">
-      <div class="column is-1 logo mt-3 p-1 is-size-4">
-        <img :src="img" :alt="title" />
+    <div
+      class="contents is-mobile is-hidden-desktop columns content mt-1 is-centered is-align-item-center"
+    >
+      <div class="column is-12">
+        <div class="columns is-mobile is-centered is-align-items-center">
+          <div class="column is-12 has-text-centered">
+            <img :src="img" :alt="title" class="logo" />
+            <span class="title pl-2 is-size-5">{{ title }} </span>
+          </div>
+        </div>
       </div>
-      <div class="column is-10 title p-0 pt-4 pl-1 is-size-4">{{ title }}</div>
     </div>
     <!-- pc -->
-    <div class="columns is-desktop is-hidden-touch pt-1">
-      <div class="column is-1 logo pl-5 pr-0">
-        <img :src="img" :alt="title" />
+    <div
+      class="contents columns is-desktop is-hidden-touch pt-1 is-centered is-align-item-center"
+    >
+      <div class="column is-12">
+        <div class="columns is-centered is-align-items-center">
+          <div class="column is-12 has-text-centered">
+            <img :src="img" :alt="title" class="logo" />
+            <span class="title pl-2">{{ title }}</span>
+          </div>
+        </div>
       </div>
-      <div class="column is-11 title pl-0">{{ title }}</div>
     </div>
   </header>
 </template>
 
 <style scoped lang="scss">
-header {
-  min-height: 50px;
-  max-height: 5vh;
+.contents {
+  min-height: 4rem;
+  max-height: 10vh;
   background-color: $primary-color;
   margin: 0 auto;
   width: 100%;
+  .logo {
+    max-height: 40px;
+    vertical-align: middle;
+  }
   .title {
     color: $text-secondary;
-  }
-  .logo {
-    img {
-      max-height: 40px;
-    }
+    word-break: keep-all;
+    vertical-align: middle;
   }
 }
 </style>
