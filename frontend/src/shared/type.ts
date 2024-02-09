9b52278a4e3b0e0f40a14063e889800fef2b9e3d
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-02-09 20:28:28 +0000

    APIインターフェースの型定義追加

--- frontend/src/shared/type.ts
+++ frontend/src/shared/type.ts
@@ -1,3 +1,11 @@
+export enum SITE {
+  QIITA = 1 as number,
+  CLASSMETHOD = 2,
+  CYBOZUSHIKI = 3,
+  SONICGARDEN = 4,
+  FREEE = 5
+}
+
 export interface Article {
   id: number;
   title: string;
@@ -12,4 +20,43 @@ export interface Article {
   ogpDescription: string | null;
   createdAt: Date;
   updatedAt: Date;
+}
+
+interface RegisteredCrawlerStats {
+  id: number;
+  requestsFinished: number | null;
+  requestsFailed: number | null;
+  retryHistogram: number[];
+  requestAvgFailedDurationMillis: number | null;
+  requestAvgFinishedDurationMillis: number | null;
+  requestsFinishedPerMinute: number | null;
+  requestsFailedPerMinute: number | null;
+  requestTotalDurationMillis: number | null;
+  requestsTotal: number | null;
+  crawlerRuntimeMillis: number | null;
+  siteId: SITE;
+}
+
+interface RegisteredBatchHistory {
+  id: number;
+  startAt: Date;
+  endAt: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface Site {
+  id: number;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface CrawlerStatsWithSite extends RegisteredCrawlerStats {
+  Site: Site;
+}
+
+export interface AppHistories {
+  batch: RegisteredBatchHistory;
+  crawlerStats: CrawlerStatsWithSite[];
 }
